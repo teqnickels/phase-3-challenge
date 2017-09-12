7d4981a45f452d0b6964b8c7d82d19e5bea427f4
Debrena McEwen <debrenamcewen@gmail.com>
2017-09-11 21:21:50 -0700

    add fail case

--- part-1/server.js
+++ part-1/server.js
@@ -9,12 +9,10 @@ app.listen(3000, () => {
     console.log('Server Working')
 });
 
-
-
 app.get('/api/days/:day', (request, response) => {
     const day = request.params.day
     response.set('content-type', 'application/text')
-
+    
     daysOfWeek = {
         monday: 1, 
         tuesday: 2, 
@@ -24,16 +22,22 @@ app.get('/api/days/:day', (request, response) => {
         saturday: 6, 
         sunday: 7 
     }
-
-    if(daysOfWeek.hasOwnProperty(`${day}`)) {
+    
+    if(!daysOfWeek.hasOwnProperty(`${day}`)) {
         response.status(200) 
         response.send(
-            'request: GET '+ `${request.path}`+ '\n'
-            + 'response: '+ `${daysOfWeek[day]}`+ '\n'
-            + 'status: '+ `${response.statusCode}`
+            'request: GET ' + `${request.path}` + '\n'
+            + 'response: ' + `${day} is not a valid day!` + '\n'
+            + 'status: ' + `${response.statusCode}` 
         )
-
-    } 
+    } else {
+        response.status(400)
+            response.send(
+                'request: GET '+ `${request.path}`+ '\n'
+                + 'response: '+ `${daysOfWeek[day]}`+ '\n'
+                + 'status: '+ `${response.statusCode}`
+            )
+    }
 });
 
 

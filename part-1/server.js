f5c8d04cef94542f2e6197a471015f01caff2d12
Debrena McEwen <debrenamcewen@gmail.com>
2017-09-11 21:13:23 -0700

    working and formatted

--- part-1/server.js
+++ part-1/server.js
@@ -9,23 +9,31 @@ app.listen(3000, () => {
     console.log('Server Working')
 });
 
-daysOfWeek = {
-    monday: 1, 
-    tuesday: 2, 
-    wednesday: 3, 
-    thursday: 4,
-    friday: 5, 
-    saturday: 6, 
-    sunday: 7 
-}
 
 
 app.get('/api/days/:day', (request, response) => {
     const day = request.params.day
-    
-    if(daysOfWeek.hasOwnProperty(`${day}`)) {
-        response.send(`${day} is there`)
+    response.set('content-type', 'application/text')
+
+    daysOfWeek = {
+        monday: 1, 
+        tuesday: 2, 
+        wednesday: 3, 
+        thursday: 4,
+        friday: 5, 
+        saturday: 6, 
+        sunday: 7 
     }
+
+    if(daysOfWeek.hasOwnProperty(`${day}`)) {
+        response.status(200) 
+        response.send(
+            'request: GET '+ `${request.path}`+ '\n'
+            + 'response: '+ `${daysOfWeek[day]}`+ '\n'
+            + 'status: '+ `${response.statusCode}`
+        )
+
+    } 
 });
 
 
